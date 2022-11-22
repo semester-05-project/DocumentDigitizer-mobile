492fd1818b22ee4c235faf39f8b47a4c6822fcec
tharsha <tharshasivapalarajah@gmail.com>
2022-11-22 19:27:45 +0530

    Version7

--- scan_me/lib/editing_page.dart
+++ scan_me/lib/editing_page.dart
@@ -1,129 +1,16 @@
-import 'dart:io';
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'camera_mode.dart';
-import 'home_page.dart';
-import 'sign_in.dart';
-import 'sign_up.dart';
 
-class EditPage extends StatelessWidget {
-
-  const EditPage({Key? key, required this.picture}) : super(key: key);
-
-  final File picture;
+class EditPage extends StatefulWidget {
 
   @override
+  State<EditPage> createState() => EditPageState();
+}
+
+class EditPageState extends State<EditPage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Editing Page"),
-        actions: [
-          IconButton(
-              onPressed: () => Navigator.of(context)
-              .push(MaterialPageRoute(builder: (_) => const SearchPage())),
-              icon: const Icon(Icons.document_scanner_rounded)
-          ),
-
-          PopupMenuButton(
-            // add icon, by default "3 dot" icon
-            // icon: Icon(Icons.book)
-            itemBuilder: (context){
-              return [
-                const PopupMenuItem<int>(
-                  value: 0,
-                  child: Text("Sign In"),
-                ),
-
-                const PopupMenuItem<int>(
-                  value: 1,
-                  child: Text("Sign Up"),
-                ),
-              ];
-            },
-                   
-            onSelected:(value){
-              if (value == 0) {
-                Navigator.of(context).pushReplacement( MaterialPageRoute(
-                    builder: (BuildContext context) => const SignIn()
-                  )
-                );
-              } else if (value == 1) {
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                    builder: (BuildContext context) => const SignUp()));
-              }
-            }
-          ),
-        ],
-      ),
-
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min, 
-          children: [
-            Image.file(
-              File(picture.path), 
-              fit: BoxFit.cover, 
-              width: 250
-            ),
-
-            const SizedBox(height: 24),
-
-            //Text(picture.name),
-
-            const Padding(
-                  padding: EdgeInsets.only(top: 25)
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children:  <Widget>[
-                FloatingActionButton(
-                  heroTag: 'button1',
-                  onPressed: () async {},
-                  tooltip: "increment",
-                  child: const Icon(Icons.backup_sharp),
-                ),
-
-                FloatingActionButton(
-                  heroTag: 'button2',
-                  onPressed: () async {},
-                  tooltip: "increment",
-                  child: const Icon(Icons.share_rounded),
-                ),
-
-                FloatingActionButton(
-                  heroTag: 'button3',
-                  onPressed: () async {
-                    await availableCameras().then(
-                      (value) => Navigator.push(
-                        context, MaterialPageRoute(
-                          builder: (_) => CameraPage(cameras: value)
-                        )
-                      )
-                    );
-                  },
-                  tooltip: "increment",
-                  child: const Icon(Icons.add_box_rounded),
-                ),
-
-                FloatingActionButton(
-                  heroTag: 'button4',
-                  onPressed: () async {},
-                  tooltip: "increment",
-                  child: const Icon(Icons.picture_as_pdf_rounded),
-                ),
-
-                FloatingActionButton(
-                  heroTag: 'button5',
-                  onPressed: () async {},
-                  tooltip: "increment",
-                  child: const Icon(Icons.delete_rounded),
-                ),
-              ],
-            ),
-          ]
-        ),
-      ),
-    );
+    // TODO: implement build
+    throw UnimplementedError();
   }
+
 }
